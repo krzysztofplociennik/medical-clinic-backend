b3f1b322e26cd1f5b661061605873e2e331813c1
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2020-05-11 16:02:24 +0100

    Added more dummy data

--- src/test/java/com/plociennik/medicalclinicbackend/DatabaseTest.java
+++ src/test/java/com/plociennik/medicalclinicbackend/DatabaseTest.java
@@ -1,4 +1,7 @@
 package com.plociennik.medicalclinicbackend;
+import com.plociennik.medicalclinicbackend.domain.Reservation;
+import com.plociennik.medicalclinicbackend.service.DoctorService;
+import com.plociennik.medicalclinicbackend.service.PatientService;
 import com.plociennik.medicalclinicbackend.service.ReservationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,15 +9,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DatabaseTest {
     @Autowired
-    private ReservationService service;
+    private ReservationService reservationService;
+    @Autowired
+    private DoctorService doctorService;
+    @Autowired
+    private PatientService patientService;
     @Test
     public void whatever() {
         //Reservation reservation = new Reservation();
         //reservationRepository.save(reservation);
-        System.out.println(service.getAllReservations().size());
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        reservationService.saveReservation(new Reservation(221L,
+                LocalDateTime.of(2020, 4, 26, 17, 0),
+                patientService.getPatientFromName("Sandra Ham"),
+                doctorService.getDoctorFromName("John Doe")));
+        System.out.println(reservationService.getAllReservations().size());
     }
 }
