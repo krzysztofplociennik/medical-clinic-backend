650886900667dd5d040f76c41a4f7c34378ff7be
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2021-03-01 08:19:21 +0100

    Cleared null date values in ratings

--- src/test/java/com/plociennik/medicalclinicbackend/other/RandomTesting.java
+++ src/test/java/com/plociennik/medicalclinicbackend/other/RandomTesting.java
@@ -1,5 +1,6 @@
 package com.plociennik.medicalclinicbackend.other;
 
+import com.plociennik.medicalclinicbackend.domain.Rating;
 import com.plociennik.medicalclinicbackend.repository.DoctorRepository;
 import com.plociennik.medicalclinicbackend.repository.PatientRepository;
 import com.plociennik.medicalclinicbackend.repository.RatingRepository;
@@ -9,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,5 +32,19 @@ public class RandomTesting {
         + "\nDoctors: " + doctorRepository.count()
         + "\nRatings: " + ratingRepository.count()
         + "\nReservations: " + reservationRepository.count());
+    }
+
+    @Test
+    public void clearNullDateDataValuesInRatings() {
+        System.out.println("Number of records: " + ratingRepository.count());
+        List<Long> indexes = ratingRepository.findAll().stream()
+                .filter(rating -> rating.getDateTime() == null)
+                .map(Rating::getId)
+                .collect(Collectors.toList());
+
+        for (Long index : indexes) {
+            ratingRepository.deleteById(index);
+        }
+        System.out.println("Number of records: " + ratingRepository.count());
     }
 }
